539ab335ff12fe63858fd840902d4f0c3ec30fb5
Torben <wilkening@nordlevel.com>
2017-04-27 13:01:16 +0200

    added rjsx mode for default js development

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,145 @@
+;;; init.el --- Prelude's configuration entry point.
+;;
+;; Copyright (c) 2011-2017 Bozhidar Batsov
+;;
+;; Author: Bozhidar Batsov <bozhidar@batsov.com>
+;; URL: http://batsov.com/prelude
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This file simply sets up the default load path and requires
+;; the various modules defined within Emacs Prelude.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
+(defvar current-user
+  (getenv
+   (if (equal system-type 'windows-nt) "USERNAME" "USER")))
+
+(message "Prelude is powering up... Be patient, Master %s!" current-user)
+
+(when (version< emacs-version "24.4")
+  (error "Prelude requires at least GNU Emacs 24.4, but you're running %s" emacs-version))
+
+;; Always load newest byte code
+(setq load-prefer-newer t)
+
+(defvar prelude-dir (file-name-directory load-file-name)
+  "The root dir of the Emacs Prelude distribution.")
+(defvar prelude-core-dir (expand-file-name "core" prelude-dir)
+  "The home of Prelude's core functionality.")
+(defvar prelude-modules-dir (expand-file-name  "modules" prelude-dir)
+  "This directory houses all of the built-in Prelude modules.")
+(defvar prelude-personal-dir (expand-file-name "personal" prelude-dir)
+  "This directory is for your personal configuration.
+
+Users of Emacs Prelude are encouraged to keep their personal configuration
+changes in this directory.  All Emacs Lisp files there are loaded automatically
+by Prelude.")
+(defvar prelude-personal-preload-dir (expand-file-name "preload" prelude-personal-dir)
+  "This directory is for your personal configuration, that you want loaded before Prelude.")
+(defvar prelude-vendor-dir (expand-file-name "vendor" prelude-dir)
+  "This directory houses packages that are not yet available in ELPA (or MELPA).")
+(defvar prelude-savefile-dir (expand-file-name "savefile" prelude-dir)
+  "This folder stores all the automatically generated save/history-files.")
+(defvar prelude-modules-file (expand-file-name "prelude-modules.el" prelude-dir)
+  "This files contains a list of modules that will be loaded by Prelude.")
+
+(unless (file-exists-p prelude-savefile-dir)
+  (make-directory prelude-savefile-dir))
+
+(defun prelude-add-subfolders-to-load-path (parent-dir)
+ "Add all level PARENT-DIR subdirs to the `load-path'."
+ (dolist (f (directory-files parent-dir))
+   (let ((name (expand-file-name f parent-dir)))
+     (when (and (file-directory-p name)
+                (not (string-prefix-p "." f)))
+       (add-to-list 'load-path name)
+       (prelude-add-subfolders-to-load-path name)))))
+
+;; add Prelude's directories to Emacs's `load-path'
+(add-to-list 'load-path prelude-core-dir)
+(add-to-list 'load-path prelude-modules-dir)
+(add-to-list 'load-path prelude-vendor-dir)
+(prelude-add-subfolders-to-load-path prelude-vendor-dir)
+
+;; reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; warn when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+;; preload the personal settings from `prelude-personal-preload-dir'
+(when (file-exists-p prelude-personal-preload-dir)
+  (message "Loading personal configuration files in %s..." prelude-personal-preload-dir)
+  (mapc 'load (directory-files prelude-personal-preload-dir 't "^[^#\.].*el$")))
+
+(message "Loading Prelude's core...")
+
+;; the core stuff
+(require 'prelude-packages)
+(require 'prelude-custom)  ;; Needs to be loaded before core, editor and ui
+(require 'prelude-ui)
+(require 'prelude-core)
+(require 'prelude-mode)
+(require 'prelude-editor)
+(require 'prelude-global-keybindings)
+
+;; OSX specific settings
+(when (eq system-type 'darwin)
+  (require 'prelude-osx))
+
+(message "Loading Prelude's modules...")
+
+;; the modules
+(if (file-exists-p prelude-modules-file)
+    (load prelude-modules-file)
+  (message "Missing modules file %s" prelude-modules-file)
+  (message "You can get started by copying the bundled example file"))
+
+;; config changes made through the customize UI will be store here
+(setq custom-file (expand-file-name "custom.el" prelude-personal-dir))
+
+;; load the personal settings (this includes `custom-file')
+(when (file-exists-p prelude-personal-dir)
+  (message "Loading personal configuration files in %s..." prelude-personal-dir)
+  (mapc 'load (directory-files prelude-personal-dir 't "^[^#\.].*el$")))
+
+(message "Prelude is ready to do thy bidding, Master %s!" current-user)
+
+(prelude-eval-after-init
+ ;; greet the use with some useful tip
+ (run-at-time 5 nil 'prelude-tip-of-the-day))
+
+;;; init.el ends here
 
 ;;; CUSTOM INIT
 
@@ -35,7 +177,7 @@
 
 ;; check if packages are installed and if not install
 (defvar own-packages
-  '(go-mode go-autocomplete android-mode flycheck js2-mode json-mode web-mode auto-complete tern-auto-complete jedi-core jedi typescript-mode tss))
+  '(go-mode go-autocomplete android-mode flycheck js2-mode json-mode web-mode auto-complete tern-auto-complete jedi-core jedi typescript-mode tss rjsx-mode))
 
 (defun own-packages-installed-p ()
   (loop for p in own-packages
@@ -47,7 +189,13 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
-
+;; dirty fix for having AC everywhere
+;(define-globalized-minor-mode real-global-auto-complete-mode
+;  auto-complete-mode (lambda ()
+;                       (if (not (minibufferp (current-buffer)))
+;                           (auto-complete-mode 1))
+;                       ))
+;(real-global-auto-complete-mode t)
 
 
 ;set default modifier keys
@@ -60,6 +208,14 @@
 
 ;; enable ac by default
 (global-auto-complete-mode t)
+;; add modes to auto-complete
+;; dirty fix for having AC everywhere
+(define-globalized-minor-mode real-global-auto-complete-mode
+  auto-complete-mode (lambda ()
+                       (if (not (minibufferp (current-buffer)))
+                           (auto-complete-mode 1))
+                       ))
+(real-global-auto-complete-mode t)
 
 ;; https://github.com/purcell/exec-path-from-shell
 ;; only need exec-path-from-shell on OSX
@@ -115,7 +271,23 @@
   (local-set-key (kbd "M-.") 'godef-jump))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
+;; PHP stuff
+(autoload 'php-mode "php-mode" "Major mode for editing PHP code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 
+
+(add-hook 'php-mode-hook
+          '(lambda ()
+             (auto-complete-mode t)
+             (require 'ac-php)
+             (setq ac-sources  '(ac-source-php ) )
+             (yas-global-mode 1)
+
+             (ac-php-core-eldoc-setup ) ;; enable eldoc
+             (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
+             (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
+             ))
 
 ;;;;;;;;;;; Typescript stuff
 ;; npm install -g typescript typescript-tools
@@ -137,15 +309,19 @@
 (tss-config-default)
 
 
-
-
 ;;;;;;;;;;; ES6 stuff
 ; npm install -g eslint babel-eslint eslint-plugin-react tern
 ; use web-mode for .jsx files
 
+
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
-(setq web-mode-content-types-alist
+;(add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
+;(setq web-mode-content-types-alist
+;      '(("jsx"  . "\\.js?\\'")))
+
+(require 'rjsx-mode)
+(add-to-list 'auto-mode-alist '("\\.js?\\'" . rjsx-mode))
+(setq rjsx-mode-content-types-alist
       '(("jsx"  . "\\.js?\\'")))
 
 ;;http://www.flycheck.org/manual/latest/index.html
@@ -158,6 +334,8 @@
                       '(javascript-jshint)))
 ;; use eslint with web-mode for jsx files
 (flycheck-add-mode 'javascript-eslint 'web-mode)
+(flycheck-add-mode 'javascript-eslint 'js2-mode)
+(flycheck-add-mode 'javascript-eslint 'rjsx-mode)
 ;; customize flycheck temp file prefix
 (setq-default flycheck-temp-prefix ".flycheck")
 ;; disable json-jsonlist checking for json files
@@ -188,12 +366,47 @@
 ;(add-hook 'web-mode-hook  'my-web-mode-hook)
 
 ;(add-to-list 'load-path "~.emacs.d/tern/")
-(require 'tern-auto-complete)
+
+;(require 'auto-complete)
+
+
+;(require 'tern-auto-complete)
+
 (autoload 'tern-mode "tern.el" nil t)
 (add-hook 'web-mode-hook (lambda () (tern-mode t)))
 
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+  '(progn
+     (require 'tern-auto-complete)
+     (tern-ac-setup)))
+
+
+
+(add-hook 'rjsx-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+  '(progn
+     (require 'tern-auto-complete)
+     (tern-ac-setup)))
+
+
+(add-hook 'web-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+  '(progn
+     (require 'tern-auto-complete)
+     (tern-ac-setup)))
+
+
+
+
+; add .tern-project :
+;{ "plugins": {"node": {}}}
+
 ;; install following emacs packages:
 ;; flycheck, js2-mode, json-mode, web-mode, (osx -> exec-path-from-shell)
+
+
+
 
 ;; python development make sure virtual-env is install and
 ;; run jedi:install-server
@@ -201,24 +414,6 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 
-;;;;;;;;;;; Typescript stuff
-;; npm install -g typescript typescript-tools
-;; If use bundled typescript.el,
-(require 'typescript)
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
-
-(require 'tss)
-
-;; Key binding
-(setq tss-popup-help-key "C-:")
-(setq tss-jump-to-definition-key "C->")
-(setq tss-implement-definition-key "C-c i")
-
-;; Make config suit for you. About the config item, eval the following sexp.
-;; (customize-group "tss")
-
-;; Do setting recommemded configuration
-(tss-config-default)
 
 (global-set-key (kbd "C-o") 'other-window)
 (global-set-key (kbd "C-p") 'prev-window)
@@ -226,6 +421,8 @@
 (defun prev-window ()
   (interactive)
   (other-window -1))
+
+
 
 
 
