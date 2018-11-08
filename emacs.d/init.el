6bf1d93f7997ad1ebfdd4fb7b6c9513e191e1434
Torben Wilkening <torbenwilkening@googlemail.com>
2018-11-08 16:47:47 +0100

    added typescript mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -106,3 +106,56 @@
 (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
 (setq company-echo-delay 0)                          ; remove annoying blinking
 (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+
+
+
+;; company mode autocompletion on first character
+(setq company-minimum-prefix-length 1)
+;; disable only lowercase in company autocompletion
+(setq company-dabbrev-downcase nil)
+;; default font size
+(set-face-attribute 'default nil :height 100)
+
+
+;; typescript stuff
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "ts" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; configure jsx-tide checker to run after your default jsx checker
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+;(flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
+;; M-: key for GoToDefinition
+;; M-; key for GoBack
+;; M-- key for FindUsages
+
+(add-hook 'tide-mode-hook
+          (lambda () (local-set-key (kbd "M-m") 'tide-references)))
+
+
+
+
+;; try this for react:
+(add-to-list 'magic-mode-alist
+             '("\\(import.*from \'react\';\\|\/\/ @flow\nimport.*from \'react\';\\)" . rjsx-mode))
