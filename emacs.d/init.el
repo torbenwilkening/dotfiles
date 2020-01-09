a53279991c8a41a6c191fdfffb20201429aeb725
torbenwilkening <torbenwilkening@googlemail.com>
2020-01-09 17:11:37 +0100

    fixed init.el and added ruby

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,105 +1,99 @@
+;;; init.el ends here
 
 
-;; melpa stuff
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 
-;;; Uncomment the modules you'd like to use and restart Prelude afterwards
-(require 'prelude-c)
-(require 'prelude-clojure)
-(require 'prelude-coffee)
-(require 'prelude-common-lisp)
-;(require 'prelude-css)
-(require 'prelude-emacs-lisp)
-(require 'prelude-erc)
-(require 'prelude-erlang)
-(require 'prelude-elixir)
-(require 'prelude-haskell)
-;(require 'prelude-js)
-(require 'prelude-latex)
-(require 'prelude-lisp)
-(require 'prelude-mediawiki)
-(require 'prelude-org)
-(require 'prelude-perl)
-;(require 'prelude-python)
-(require 'prelude-ruby)
-(require 'prelude-scala)
-(require 'prelude-scheme)
-;(require 'prelude-scss)
-;(require 'prelude-web)
-(require 'prelude-xml)
+;; default stuff
+
+(eval-when-compile (require 'use-package))
+(use-package company :ensure t)
+(scroll-bar-mode -1)
+;; some company tweaks
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-idle-delay .1)                         ; decrease delay before autocompletion popup shows
+(setq company-echo-delay 0)                          ; remove annoying blinking
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+;; company mode autocompletion on first character
+(setq company-minimum-prefix-length 0)
+;; disable only lowercase in company autocompletion
+(setq company-dabbrev-downcase nil)
+;; disable whitespace mode by default
+(global-whitespace-mode 0)
+(whitespace-mode 0)
+;; default size
+(when window-system
+  (set-frame-position (selected-frame) 0 0)
+  (set-frame-size (selected-frame) 140 60))
+;; for osx keyboards
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+(global-set-key (kbd "s-l") '(lambda () (interactive) (insert "@")))
 
 
-;; install packages if not exists:
 
-;;(unless (package-installed-p 'indium)
-;;  (package-install 'indium))
+
+;; ruby
+(use-package ruby-mode :ensure t)
+(use-package flymake-ruby :ensure t)
+(use-package projectile-rails :ensure t)
+
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:cap\\|gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist
+             '("\\(?:Brewfile\\|Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+
+(if (not (getenv "TERM_PROGRAM"))
+    (setenv "PATH"
+            (shell-command-to-string "source $HOME/.zshrc && printf $PATH")))
+(projectile-rails-global-mode)
+
+(add-hook 'ruby-mode-hook 'flymake-ruby-load)
+(global-set-key (kbd "C-c r r") 'inf-ruby)
+
+(setq seeing-is-believing-prefix "C-,")
+(add-hook 'ruby-mode-hook 'seeing-is-believing)
+(use-package seeing-is-believing :ensure t)
+
+(add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
+
+(use-package ruby-test-mode :ensure t)
+(add-hook 'ruby-mode-hook 'ruby-test-mode)
+;;(lambda (buf strg)
+;;  (switch-to-buffer-other-window "*compilation*")
+;;  (read-only-mode)
+;;  (goto-char (point-max))
+;;  (local-set-key (kbd "q")
+;;                 (lambda () (interactive) (quit-restore-window)))))
+
+;; robe for company and jump to
+(add-hook 'ruby-mode-hook 'robe-mode)
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
+
 
 ;; python stuff
-(require 'company)
-(require 'company-web-html)
-(require 'company-tern)
-(require 'company-go)
-(require 'js2-mode)
-(require 'web-mode)
-(require 'grails)
-(require 'company-jedi)
-(require 'indium)
-(require 'rjsx-mode)
-(require 'ensime)
-(require 'scala-mode)
-
-
-
-
-;; scala stuff
-
-
-
-;; python stuff
+(use-package company-jedi :ensure t)
 (defun my-python-mode-hook ()
   (add-to-list 'company-backends 'company-jedi)
   development)
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 
-
 ;; scala stuff
+(use-package scala-mode :ensure t)
 (add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
-;;(defun my-scala-mode-hook ()
-;;  (ensime))
-;;(add-hook 'scala-mode-hook 'my-scala-mode-hook)
 
-;; put this to ~/.sbt/1.0/plugins.sbt
-;;addSbtPlugin("org.ensime" % "sbt-ensime" % "2.5.1")
-;;ensimeIgnoreScalaMismatch in ThisBuild := true
-
-
-;; js2-mode stuff (node)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; node stuff (js3-mode)
+(use-package js3-mode :ensure t)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js3-mode))
 (add-to-list 'company-backends 'company-tern)
-(add-hook 'js2-mode-hook (lambda ()
+(add-hook 'js3-mode-hook (lambda ()
                            (tern-mode)
                            (company-mode)))
 
-
-;; js2-jsx-mode stuff (react)
-(add-to-list 'magic-mode-alist
-             '("\\(import.*from \'react\';\\|\/\/ @flow\nimport.*from \'react\';\\)" . js2-jsx-mode))
-(flycheck-add-mode 'javascript-eslint 'js2-mode)
-(add-hook 'js2-jsx-mode-hook (lambda ()
-                           (tern-mode)
-                           (company-mode)
-                           (rjsx-minor-mode)))
-(flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
-
-;; indium js debugger
-;; and setup your project : https://indium.readthedocs.io/en/latest/setup.html
-(add-hook 'js2-mode-hook #'indium-interaction-mode)
-
-
 ;; webmode stuff
+(use-package web-mode)
 (defun my-web-mode-hook ()
   "Hook for `web-mode'."
   (set (make-local-variable 'company-backends)
@@ -120,55 +114,19 @@
                           (unless tern-mode (tern-mode))
                         (if tern-mode (tern-mode -1)))))))
 
-
-
-
-
 ;; go stuff
+(use-package go-mode)
 (add-hook 'go-mode-hook (lambda ()
                           (set (make-local-variable 'company-backends) '(company-go))
                           (company-mode)))
 
-
-
-
-;; typescript stuff
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  (company-mode +1))
-
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-
-;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
-(add-hook 'web-mode-hook
+;; some sql mode fixes
+(add-hook 'sql-interactive-mode-hook
           (lambda ()
-            (when (string-equal "ts" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
-;; configure jsx-tide checker to run after your default jsx checker
-(flycheck-add-mode 'javascript-eslint 'web-mode)
-;(flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
-;; M-: key for GoToDefinition
-;; M-; key for GoBack
-;; M-- key for FindUsages
-(add-hook 'tide-mode-hook
-          (lambda () (local-set-key (kbd "M-m") 'tide-references)))
+            (toggle-truncate-lines t)))
 
-
-
-
-; some .tern-project file example for ~/.tern-project
-;{
+;; some .tern-project file example for ~/.tern-project
+;;{
 ;; "libs": [
 ;;          "browser",
 ;;          "jquery",
@@ -206,37 +164,3 @@
 ;; },
 ;; "ecmaVersion": 6
 ;; }
-
-
-;; default emacs tweaks
-(scroll-bar-mode -1)
-
-;; some company tweaks
-(setq company-tooltip-limit 20)                      ; bigger popup window
-(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
-(setq company-echo-delay 0)                          ; remove annoying blinking
-(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
-;; company mode autocompletion on first character
-(setq company-minimum-prefix-length 1)
-;; disable only lowercase in company autocompletion
-(setq company-dabbrev-downcase nil)
-
-;; shortcuts
-(set-face-attribute 'default nil :height 100)
-;(setq global-map (make-keymap))
-
-;;(define-key (current-global-map) (kbd "C-o") 'other-window)
-;;(define-key (current-global-map) [remap kill-line] 'my-homemade-kill-line)
-;;(global-set-key (kbd "C-o") 'other-window)
-;;(global-set-key (kbd "C-p") 'prev-window)
-;;(defun prev-window ()
-;;  (interactive)
-;;  (other-window -1))
-
-;; path variable
-(add-to-list 'exec-path "/usr/local/bin")
-
-;; some sql mode fixes
-(add-hook 'sql-interactive-mode-hook
-          (lambda ()
-            (toggle-truncate-lines t)))
